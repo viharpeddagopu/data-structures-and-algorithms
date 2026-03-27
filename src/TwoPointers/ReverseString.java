60ee48695e6dfbb56fee4d6b1fdc6244677d05d8
vihar <viharpeddagopu@gmail.com>
2026-03-27 17:37:27 +0530

    day 7 : blind75

---
+++ src/TwoPointers/ReverseString.java
@@ -0,0 +1,26 @@
+package TwoPointers;
+
+public class ReverseString {
+    public static String reversedString(String s){
+        int left = 0, right = s.length()-1;
+        char[] c = s.toCharArray();
+
+        while(left < right){
+            char temp = c[left];
+            c[left] = c[right];
+            c[right] = temp;
+
+            left++;
+            right--;
+        }
+        return new String(c);
+    }
+
+    public static void main(String[] args) {
+        String str = "Vihar";
+
+        System.out.println("reversed string is: " +reversedString(str));
+    }
+
+
+}
