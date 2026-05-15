0aee75e638eff9d3f12a6d01d1465176fae81666
vihar <viharpeddagopu@gmail.com>
2026-05-15 12:39:23 +0530

    blind75: linked list implemented

---
+++ src/LinkedList/LinkedListImplementation.java
@@ -0,0 +1,89 @@
+package LinkedList;
+
+public class LinkedListImplementation {
+    //node class
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    //head of the list
+    Node head;
+
+    //insert at the end
+    public void insert(int data){
+        Node newNode = new Node(data);
+
+        if (head == null){
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+
+        while(temp.next != null)
+            temp = temp.next;
+
+        temp.next = newNode;
+    }
+
+    //insert at beginning
+    public void insertAtBeginning(int data){
+        Node newNode = new Node(data);
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    //delete by value
+    public void deleteByValue(int data){
+        if (head == null) {
+            return;
+        }
+
+        if (head.data == data){
+            head.next = head;
+            return;
+        }
+
+        Node temp = head;
+
+        while(temp.next!=null && temp.next.data!=data)
+            temp = temp.next;
+
+        if (temp.next == null)
+            return;
+
+        temp.next = temp.next.next;
+    }
+
+    //search element
+    public boolean findElement(int data){
+        Node temp = head;
+
+        while(temp != null){
+            if (temp.data == data)
+                return true;
+
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    //print linked list
+    public void printList(){
+        Node temp = head;
+
+        while(temp != null){
+            System.out.println(temp.data + "->");
+
+            temp = temp.next;
+        }
+        System.out.println(null);
+    }
+}
