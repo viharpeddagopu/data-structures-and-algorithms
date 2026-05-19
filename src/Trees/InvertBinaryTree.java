64c53f4ff739aeddc16455bbaf7462ef8e4821d0
vihar <viharpeddagopu@gmail.com>
2026-05-19 12:18:47 +0530

    blind75: Inverting a binary tree

--- src/Trees/InvertBinaryTree.java
+++ src/Trees/InvertBinaryTree.java
@@ -1,4 +1,44 @@
 package Trees;
 
 public class InvertBinaryTree {
+
+    public static void invertTree(Node root){
+
+        // base case
+        if (root == null)
+            return;
+
+        // swap children
+        Node temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        // recursively invert subtrees
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return;
+    }
+
+    public static void main(String[] args) {
+
+        BinarySearchTree tree =
+                new BinarySearchTree();
+
+        tree.insert(new Node(4));
+        tree.insert(new Node(2));
+        tree.insert(new Node(7));
+        tree.insert(new Node(1));
+        tree.insert(new Node(3));
+        tree.insert(new Node(6));
+        tree.insert(new Node(9));
+
+        System.out.println("Before inversion:");
+        tree.display();
+
+        invertTree(tree.root);
+
+        System.out.println("\nAfter inversion:");
+        tree.display();
+    }
 }
